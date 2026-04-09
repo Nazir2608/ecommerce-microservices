13f91fbe3462d6040796b0d2b340fe7a4e31be6c
Md Nazir <mdnazir2608@gmail.com>
2026-04-09 22:48:55 +0530

    refactored

--- product-service/src/main/java/com/nazir/ecommerce/productservice/config/MongoConfig.java
+++ product-service/src/main/java/com/nazir/ecommerce/productservice/config/MongoConfig.java
@@ -16,6 +16,4 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @EnableMongoAuditing
 @EnableMongoRepositories(basePackages = "com.nazir.ecommerce.productservice.repository")
 public class MongoConfig {
-    // MongoDB connection is auto-configured from spring.data.mongodb.uri in application.yml
-    // Add custom converters here if needed (e.g. for complex type mappings)
 }
